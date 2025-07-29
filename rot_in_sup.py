ddc811e82fac497dcb203a2c887d1546e6bdd8c8
Linda <linda.linsefors@gmail.com>
2025-07-29 19:34:26 +0100

    Replacing batch_size witgh bs. Replacing both D_over_d and D with Dod.

--- rot_in_sup.py
+++ rot_in_sup.py
@@ -45,12 +45,12 @@ except:
 
 #%% Assignments for circuits in a superposition
 
-def get_steps(S, D_over_d):
+def get_steps(S, Dod):
 
   primes_smaller_than_S = [num for num in range(2,S) if isprime(num)]
 
   step = 1
-  while step * (S-1) < D_over_d:
+  while step * (S-1) < Dod:
 
     for p in primes_smaller_than_S:
       if step % p == 0:
@@ -59,7 +59,7 @@ def get_steps(S, D_over_d):
       yield step
       if not isprime(S):
         n=1
-        while step * S**n * (S-1) < D_over_d:
+        while step * S**n * (S-1) < Dod:
           yield step * S**n
           n+=1
     step += 1
@@ -69,10 +69,10 @@ class MaxT(Exception):
   pass
 
 
-def maxT(D_over_d = 500, S = 5):
+def maxT(Dod = 500, S = 5):
   t = 0
 
-  steps = get_steps(S, D_over_d)
+  steps = get_steps(S, Dod)
   try:
     step = next(steps)
   except StopIteration:
@@ -83,10 +83,10 @@ def maxT(D_over_d = 500, S = 5):
 
   while True:
 
-    if i + step*(S-1) >= D_over_d:
+    if i + step*(S-1) >= Dod:
       shift += 1
 
-      if shift >= step or shift + step*(S-1) >= D_over_d:
+      if shift >= step or shift + step*(S-1) >= Dod:
         try:
           step = next(steps)
         except StopIteration:
@@ -100,10 +100,10 @@ def maxT(D_over_d = 500, S = 5):
     t += 1
 
 
-def comp_in_sup_assignment(T = 2000, D_over_d = 500, S = 5, device="cpu"):
+def comp_in_sup_assignment(T = 2000, Dod = 500, S = 5, device="cpu"):
 
-  D_over_d = int(D_over_d)
-  assignments = torch.zeros(T, D_over_d, dtype=torch.int64)
+  Dod = int(Dod)
+  assignments = torch.zeros(T, Dod, dtype=torch.int64)
   compact_assignments = torch.zeros(T, S, dtype=torch.int64)
 
   if S == 1:
@@ -112,21 +112,21 @@ def comp_in_sup_assignment(T = 2000, D_over_d = 500, S = 5, device="cpu"):
       assignments[t,i] = 1
       compact_assignments[t,0] = i
       i += 1
-      if i >= D_over_d:
+      if i >= Dod:
         i = 0
     return assignments.to(device).float(), compact_assignments.to(device).int()
 
-  steps = get_steps(S, D_over_d)
+  steps = get_steps(S, Dod)
   step = next(steps)
   shift=0
   i=0
 
   for t in range(T):
 
-    if i + step*(S-1) >= D_over_d:
+    if i + step*(S-1) >= Dod:
       shift += 1
 
-      if shift >= step or shift + step*(S-1) >= D_over_d:
+      if shift >= step or shift + step*(S-1) >= Dod:
         try:
           step = next(steps)
         except StopIteration:
@@ -189,15 +189,15 @@ class RunData:
    pass
 
 class RotInSupNetwork:
-    def __init__(self, D=1000, T=6000, S=5, device="cpu"):
+    def __init__(self, Dod=1000, T=6000, S=5, device="cpu"):
 
         #Function parameters
-        D = int(D) # Number of neurons in the large network devided by 4
+        Dod = int(Dod) # Number of neurons in the large network devided by 4
         T = int(T) # Number of small circuits in superposition
         S = int(S) # Number of large network neurons used by each small circuit neuron
 
         #Embedding assignments
-        assignments_1, compact_assignments_1 = comp_in_sup_assignment(T, D, S, device)
+        assignments_1, compact_assignments_1 = comp_in_sup_assignment(T, Dod, S, device)
         shuffle = torch.randperm(T, device=device)
         assignments_2 = assignments_1[shuffle]
         compact_assignments_2 = compact_assignments_1[shuffle]
@@ -216,40 +216,40 @@ class RotInSupNetwork:
         one = torch.ones(2, device=device)
 
         #Large network weight matrices
-        W1 = torch.zeros(4*D, 4*D, device=device)
-        W2 = torch.zeros(4*D, 4*D, device=device)
+        W1 = torch.zeros(4*Dod, 4*Dod, device=device)
+        W2 = torch.zeros(4*Dod, 4*Dod, device=device)
 
         #Perserveing activationi indicators
-        W1[:2*D, :2*D] = torch.eye(2*D, device=device)
-        W2[:2*D, :2*D] = torch.eye(2*D, device=device)
+        W1[:2*Dod, :2*Dod] = torch.eye(2*Dod, device=device)
+        W2[:2*Dod, :2*Dod] = torch.eye(2*Dod, device=device)
 
         #Adding 2 to active circuit neurons
-        W1[2*D:3*D, :D] = torch.eye(D, device=device) * 2
-        W1[3*D:4*D, :D] = torch.eye(D, device=device) * 2
-        W2[2*D:3*D, D:2*D] = torch.eye(D, device=device) * 2
-        W2[3*D:4*D, D:2*D] = torch.eye(D, device=device) * 2
+        W1[2*Dod:3*Dod, :Dod] = torch.eye(Dod, device=device) * 2
+        W1[3*Dod:4*Dod, :Dod] = torch.eye(Dod, device=device) * 2
+        W2[2*Dod:3*Dod, Dod:2*Dod] = torch.eye(Dod, device=device) * 2
+        W2[3*Dod:4*Dod, Dod:2*Dod] = torch.eye(Dod, device=device) * 2
 
         #Removing rotated one
         rotated_one = torch.einsum('tn,tm,tij,j->nmi', (assignments_1, assignments_2, r, one))/S
-        W1[2*D:3*D, D:2*D] = - rotated_one[:,:,0]
-        W1[3*D:4*D, D:2*D] = - rotated_one[:,:,1]
-        W2[2*D:3*D, :D] = - rotated_one[:,:,0].t()
-        W2[3*D:4*D, :D] = - rotated_one[:,:,1].t()
+        W1[2*Dod:3*Dod, Dod:2*Dod] = - rotated_one[:,:,0]
+        W1[3*Dod:4*Dod, Dod:2*Dod] = - rotated_one[:,:,1]
+        W2[2*Dod:3*Dod, :Dod] = - rotated_one[:,:,0].t()
+        W2[3*Dod:4*Dod, :Dod] = - rotated_one[:,:,1].t()
 
         #Rotation
         all_rotations= torch.einsum('tn,tm,tij->nmij', (assignments_1, assignments_2, r))/S
-        W1[2*D:3*D, 2*D:3*D] = all_rotations[:,:,0,0]
-        W1[2*D:3*D, 3*D:4*D] = all_rotations[:,:,0,1]
-        W1[3*D:4*D, 2*D:3*D] = all_rotations[:,:,1,0]
-        W1[3*D:4*D, 3*D:4*D] = all_rotations[:,:,1,1]
-        W2[2*D:3*D, 2*D:3*D] = all_rotations[:,:,0,0].t()
-        W2[2*D:3*D, 3*D:4*D] = all_rotations[:,:,0,1].t()
-        W2[3*D:4*D, 2*D:3*D] = all_rotations[:,:,1,0].t()
-        W2[3*D:4*D, 3*D:4*D] = all_rotations[:,:,1,1].t()
+        W1[2*Dod:3*Dod, 2*Dod:3*Dod] = all_rotations[:,:,0,0]
+        W1[2*Dod:3*Dod, 3*Dod:4*Dod] = all_rotations[:,:,0,1]
+        W1[3*Dod:4*Dod, 2*Dod:3*Dod] = all_rotations[:,:,1,0]
+        W1[3*Dod:4*Dod, 3*Dod:4*Dod] = all_rotations[:,:,1,1]
+        W2[2*Dod:3*Dod, 2*Dod:3*Dod] = all_rotations[:,:,0,0].t()
+        W2[2*Dod:3*Dod, 3*Dod:4*Dod] = all_rotations[:,:,0,1].t()
+        W2[3*Dod:4*Dod, 2*Dod:3*Dod] = all_rotations[:,:,1,0].t()
+        W2[3*Dod:4*Dod, 3*Dod:4*Dod] = all_rotations[:,:,1,1].t()
 
         #Saving data to network object
         self.device = device
-        self.D = D
+        self.Dod = Dod
         self.T = T
         self.S = S
         self.assignments_1 = assignments_1
@@ -265,16 +265,16 @@ class RotInSupNetwork:
         self.run_by_name = {}
 
 
-    def run(self, L = 2, z = 2, batch_size = 2, run_name = None):
+    def run(self, L = 2, z = 2, bs = 2, run_name = None):
 
         #Function parameters
         L = int(L) # Number of layers
         z = int(z) # Number of circuits in superposition
-        batch_size = int(batch_size) # Batch size
+        bs = int(bs) # Batch size
 
         #Import network data as local variables
         device = self.device
-        D = self.D
+        Dod = self.Dod
         T = self.T
         S = self.S
         assignments_1 = self.assignments_1
@@ -284,17 +284,17 @@ class RotInSupNetwork:
         W2 = self.W2
 
         #Bias
-        B = torch.zeros(4*D, device=device)
-        B[2*D:] = -1
+        B = torch.zeros(4*Dod, device=device)
+        B[2*Dod:] = -1
 
         #Neuron activations (batched)
-        A = torch.zeros(L, batch_size, 4*D, device=device)
-        x = torch.zeros(L, batch_size, z, 2, device=device)
-        est_x = torch.zeros(L, batch_size, z, 2, device=device)
+        A = torch.zeros(L, bs, 4*Dod, device=device)
+        x = torch.zeros(L, bs, z, 2, device=device)
+        est_x = torch.zeros(L, bs, z, 2, device=device)
 
         #Inputs
-        active_circuits = torch.randint(T, (batch_size, z), device=device)
-        initial_angle = torch.rand(batch_size, z, device=device) * 2 * np.pi
+        active_circuits = torch.randint(T, (bs, z), device=device)
+        initial_angle = torch.rand(bs, z, device=device) * 2 * np.pi
         x[0, :, :, 0] = torch.cos(initial_angle)
         x[0, :, :, 1] = torch.sin(initial_angle)
         
@@ -305,15 +305,15 @@ class RotInSupNetwork:
             x[l] = torch.einsum('btij,btj->bti', r[active_circuits], x[l-1])
 
         #Large network initial values
-        A[0, :, :D] = torch.einsum('bti->bi', assignments_1[active_circuits])
-        A[0, :, D:2*D] = torch.einsum('bti->bi', assignments_2[active_circuits])
-        A[0, :, 2*D:3*D] = torch.einsum('bt,bti->bi', (x[1,:,:,0] + 1, assignments_1[active_circuits]))
-        A[0, :, 3*D:4*D] = torch.einsum('bt,bti->bi', (x[1,:,:,1] + 1, assignments_1[active_circuits])) 
+        A[0, :, :Dod] = torch.einsum('bti->bi', assignments_1[active_circuits])
+        A[0, :, Dod:2*Dod] = torch.einsum('bti->bi', assignments_2[active_circuits])
+        A[0, :, 2*Dod:3*Dod] = torch.einsum('bt,bti->bi', (x[1,:,:,0] + 1, assignments_1[active_circuits]))
+        A[0, :, 3*Dod:4*Dod] = torch.einsum('bt,bti->bi', (x[1,:,:,1] + 1, assignments_1[active_circuits])) 
 
 
         #Running the large netowrk: Layer 1
-        A[1, :, :2*D] = - torch.relu(- A[0, :, :2*D] + 1) + 1 #implements min[1,x] = -ReLU(-x+1)+1
-        A[1, :, 2*D:] = torch.relu(A[0, :, 2*D:])             #just copies over the values
+        A[1, :, :2*Dod] = - torch.relu(- A[0, :, :2*Dod] + 1) + 1 #implements min[1,x] = -ReLU(-x+1)+1
+        A[1, :, 2*Dod:] = torch.relu(A[0, :, 2*Dod:])             #just copies over the values
 
         #Running the large network: All other layers
         for l in range(2,L):
@@ -326,20 +326,20 @@ class RotInSupNetwork:
         for l in range(1,L):
             if l%2 == 1: # Odd layers
                 est_x[l,:,:,0] = torch.einsum('btn,bn->bt', 
-                                            assignments_1[active_circuits], A[l, :, 2*D:3*D])/S - 1
+                                            assignments_1[active_circuits], A[l, :, 2*Dod:3*Dod])/S - 1
                 est_x[l,:,:,1] = torch.einsum('btn,bn->bt',  
-                                            assignments_1[active_circuits], A[l, :, 3*D:4*D])/S - 1
+                                            assignments_1[active_circuits], A[l, :, 3*Dod:4*Dod])/S - 1
             else: # Even layers
                 est_x[l,:,:,0] = torch.einsum('btn,bn->bt', 
-                                            assignments_2[active_circuits], A[l, :, 2*D:3*D])/S - 1
+                                            assignments_2[active_circuits], A[l, :, 2*Dod:3*Dod])/S - 1
                 est_x[l,:,:,1] = torch.einsum('btn,bn->bt', 
-                                            assignments_2[active_circuits], A[l, :, 3*D:4*D])/S - 1
+                                            assignments_2[active_circuits], A[l, :, 3*Dod:4*Dod])/S - 1
 
         # Saving run data
         run = RunData()
         run.L = L
         run.z = z
-        run.batch_size = batch_size
+        run.bs = bs
         run.active_circuits = active_circuits
         run.x = x
         run.est_x = est_x
@@ -357,18 +357,18 @@ class RotInSupNetwork:
 
 #%% Small test
 #   Small test
-D=5
+Dod=5
 S=2
 T=2
 L=4
 z=1
-batch_size = 2
+bs = 2
 
-smal_test_net = RotInSupNetwork(D,T,S)
-test_run = smal_test_net.run(L,z,batch_size)
+smal_test_net = RotInSupNetwork(Dod,T,S)
+test_run = smal_test_net.run(L,z,bs)
 
 for k in range(z):
-    for b in range(batch_size):
+    for b in range(bs):
         print(f'\n circuit {k} in batch {b}\n')
         for l in range(L):
             print(f'l={l}')
@@ -378,18 +378,18 @@ for k in range(z):
 
 #%% Large test
 #   Large test
-D=500
+Dod=500
 S=5
 T=3000
 L=5
 z=2
-batch_size = 2
+bs = 2
 
-smal_test_net = RotInSupNetwork(D,T,S)
-test_run = smal_test_net.run(L,z,batch_size)
+smal_test_net = RotInSupNetwork(Dod,T,S)
+test_run = smal_test_net.run(L,z,bs)
 
 for k in range(z):
-    for b in range(batch_size):
+    for b in range(bs):
         print(f'\n circuit {k} in batch {b}\n')
         for l in range(L):
             print(f'l={l}')
