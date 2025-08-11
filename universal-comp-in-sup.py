e6079f3ff1e53ea12bf7d554b019cc15f11dbcdd
Linda <linda.linsefors@gmail.com>
2025-08-11 21:09:23 +0100

    Enhance CompInSup class to support capped correction and improve pre-activation handling. Add expected_mse and plot_mse functions for better MSE analysis.

--- universal-comp-in-sup.py
+++ universal-comp-in-sup.py
@@ -85,8 +85,11 @@ class RotSmallCircuits:
         return a, active_circuits
 
 class CompInSup:
-    def __init__(self, D, L, S, small_circuits, correction=None, device=device):
+    def __init__(self, D, L, S, small_circuits, correction=None, capped=False, device=device):
         self.device = device
+
+        if capped:
+            correction = 0
 
         d = small_circuits.d  # Number of neurons in each small circuit
         T = small_circuits.T
@@ -119,11 +122,28 @@ class CompInSup:
 
         W = torch.zeros(L, D, D, device=device)
         W[0] = torch.eye(D, device=device)
-        for l in range(1,L):
-            [[W[l,:Dod,     :Dod], W[l,:Dod,     Dod:2*Dod], W[l,:Dod,     2*Dod:]],
-             [W[l,Dod:2*Dod,:Dod], W[l,Dod:2*Dod,Dod:2*Dod], W[l,Dod:2*Dod,2*Dod:]],
-             [W[l,2*Dod:,   :Dod], W[l,2*Dod:,   Dod:2*Dod], W[l,2*Dod:,   2*Dod:]]
-            ]= torch.einsum('tn,tij,tm->ijnm', unemb[l], w, embed[l-1])
+
+        if not capped:
+            for l in range(1,L):
+                [[W[l,:Dod,     :Dod], W[l,:Dod,     Dod:2*Dod], W[l,:Dod,     2*Dod:]],
+                [W[l,Dod:2*Dod,:Dod], W[l,Dod:2*Dod,Dod:2*Dod], W[l,Dod:2*Dod,2*Dod:]],
+                [W[l,2*Dod:,   :Dod], W[l,2*Dod:,   Dod:2*Dod], W[l,2*Dod:,   2*Dod:]]
+                ]= torch.einsum('tn,tij,tm->ijnm', unemb[l], w, embed[l-1])
+
+        else:
+            capped_embed = torch.einsum('tn,tm->nm',embed[l],embed[l-1])
+            capped_embed.clamp_(max=1.0)
+
+            mean_w = w.mean(dim=0)
+            diff_w = w - mean_w[None,:,:]
+            for l in range(1,L):
+                [[W[l,:Dod,     :Dod], W[l,:Dod,     Dod:2*Dod], W[l,:Dod,     2*Dod:]],
+                [W[l,Dod:2*Dod,:Dod], W[l,Dod:2*Dod,Dod:2*Dod], W[l,Dod:2*Dod,2*Dod:]],
+                [W[l,2*Dod:,   :Dod], W[l,2*Dod:,   Dod:2*Dod], W[l,2*Dod:,   2*Dod:]]
+                ]= (torch.einsum('tn,tij,tm->ijnm', embed[l], diff_w, embed[l-1])
+                    + torch.einsum('nm,ij->ijnm', capped_embed, mean_w)
+                    )/S
+                    
 
         #First bias is zero, the rest are the biases of the small circuits
         B = torch.zeros(L, D, device=device)
@@ -146,10 +166,14 @@ class CompInSup:
         Dod = self.D // 3
         
         A = torch.zeros(L+1, bs, self.D, device=device)
+        pre_A = torch.zeros(L+1, bs, self.D, device=device)
+
         [A[0,:,:Dod], A[0,:,Dod:2*Dod], A[0,:,2*Dod:]] = torch.einsum('btn,bti->ibn', self.embed[0,active_circuits],a[1])
+        pre_A[0] = A[0]
+
         for l in range(L):
-            A[l+1] = torch.einsum('nm,bm->bn', self.W[l], A[l]) + self.B[l]
-            A[l+1] = torch.relu(A[l+1])
+            pre_A[l+1] = torch.einsum('nm,bm->bn', self.W[l], A[l]) + self.B[l]
+            A[l+1] = torch.relu(pre_A[l+1])
 
         est_a = torch.zeros(L+1, bs, z, 3, device=device)
         est_a[0] = a[0]
@@ -167,12 +191,68 @@ class CompInSup:
         run.est_x = est_x
         run.active_circuits = active_circuits
         run.A = A
+        run.pre_A = pre_A
         return run
+    
+
+def expected_mse(T, Dod, l, b):
+    if l == 0:
+        return (0,0)
+    
+    mse_on = l * (z-1)/Dod + (l-1)*(1+b) * z*T/Dod**2
+    mse_x = l * (z-1)/Dod + (l-1)*(1)  * z*T/Dod**2
+
+    return (mse_on, mse_x)
+    
+    
+
+def plot_mse(labels, runs, title, expected=None):
+    """Plot the mean squared error for a set of runs."""
+    
+    mse_x = []
+    mse_on = []
+    for run in runs:
+        mse_x.append((run.x - run.est_x).pow(2).mean(dim=(1, 2)).sum(dim=-1).cpu().numpy())
+        mse_on.append((run.a[:,:,:,0] - run.est_a[:,:,:,0]).pow(2).mean(dim=(1, 2)).cpu().numpy())
+
+  
+    fig = plt.figure(figsize=(10, 5))
+
+    plt.subplot(1, 2, 1)
+    for i, label in enumerate(labels):
+        line, = plt.plot(mse_on[i], marker='o', label=label)
+        if expected is not None:
+            plt.plot([expected[i][l][0] for l in range(L+1)], 
+                     linestyle='--', color=line.get_color(), marker='x')
+    plt.title('Active Circuits On-Indicator')
+    plt.xlabel('Layer')
+    plt.ylabel('Mean Squared Error')
+    plt.grid(True)
+    plt.legend()
+
+    plt.subplot(1, 2, 2)
+    for i, label in enumerate(labels):
+        line, = plt.plot(mse_x[i], marker='o', label=label)
+        if expected is not None:
+            plt.plot([expected[i][l][1] for l in range(L+1)], 
+                     linestyle='--', color=line.get_color(), marker='x')
+    plt.title('Active Circuits Rotated Vector')
+    plt.xlabel('Layer')
+    plt.ylabel('Mean Squared Error')
+    plt.grid(True)
+    plt.legend()
+
+    fig.suptitle(title, fontsize=16)
+    plt.tight_layout()
+    plt.subplots_adjust(top=0.88)  # Make room for the title
+
+    plt.show()
+
 # %% Very small test
 #    Very small test
 
 Dod=5
-D=Dod*3
+D=Dod*3#
 S=2
 T=2
 L=4
@@ -189,8 +269,130 @@ else:
     print("CompInSup test passed: The output matches the expected result.")
 
 
+#%% Plot MSE
+#   Plot MSE
 
-#%% 
+D = 1200
+T = 1000
+
+S = 5
+z = 3
+bs = 800
+L = 4
+Dod = D // 3
+b = 1
+S = 5
+z = 1
+
+f = frequency_of_overlap(T, Dod, S)
+p = probability_of_overlap(T, Dod, S)
+
+
+#correction = f/((S-f)*S)
+#correction = p/((S-p)*S)
+correction = 1/(Dod-S)
+
+
+runs = []
+labels = []
+expected = []
+
+# for correction_type in [ 'p', 'f', 'D']:
+#     if correction_type == 'p':
+#         correction = p/((S-p)*S)
+#     if correction_type == 'f':
+#         correction = f/((S-f)*S)
+#     if correction_type == 'D':
+#         correction = 1/(Dod-S)
+
+# for b in [0.3, 0.4, 0.5]:
+#     for S in [3,4,5]:
+
+#for correction in [0, 0.0021, 0.0022, 0.0023, 0.0024, 0.0025, 0.0026, 0.0027, 0.0028]:
+
+for capped in [False, True]:
+    for z in [1, 2, 3]:
+        circ = RotSmallCircuits(T, b, device=device)
+        net = CompInSup(D, L, S, circ, correction=correction, capped=capped, device=device)
+        run = net.run(L, z, bs)
+
+        runs.append(run)
+        #labels.append(f'corr type={correction_type}')
+        #labels.append(f'b={b}, S={S}')
+        labels.append(f'z={z}, capped={capped}')
+        #labels.append(f'corr={correction}')
+
+        expected.append([expected_mse(T,Dod,l,b) for l in range(L+1)]) 
+
+# title = f'D={D}, D/d = {Dod}, T={T}, L={L}, z={z}, bs={bs}, S={S}, b={b}'
+# title = f'D={D}, D/d = {Dod}, T={T}, L={L}, z={z}, bs={bs}, corr type = D'
+title = f'D={D}, D/d = {Dod}, T={T}, L={L}, bs={bs}, S={S}, b={b}'
+
+plot_mse(labels, runs, title, expected)
+
+#%%
+D = 1200
+T = 1000
+
+S = 5
+z = 3
+bs = 800
+L = 3
+Dod = D // 3
+b = 1
+
+correction = 1/(Dod-S)
+
+circ = RotSmallCircuits(T, b, device=device)
+net = CompInSup(D, L, S, circ, correction=correction, device=device)
+run = net.run(L, z, bs)
+
+embed = net.embed
+active_circuits = run.active_circuits
+pre_A = run.pre_A
+
+for l in range(1, L):
+    mask = torch.einsum('btn->bn', embed[l-1, active_circuits]) > 0
+
+    act_pre_A_on = pre_A[l,:,:Dod][mask]
+    ina_pre_A_on = pre_A[l,:,:Dod][~mask]
+
+    act_pre_A_x = pre_A[l,:,Dod:2*Dod][mask]
+    ina_pre_A_x = pre_A[l,:,Dod:2*Dod][~mask]
+
+    act_pre_A_y = pre_A[l,:,2*Dod:3*Dod][mask]
+    ina_pre_A_y = pre_A[l,:,2*Dod:3*Dod][~mask]
+
+    plt.subplot(2, 1, 1)
+    plt.title(f'Pre-Activation at layer {l}')
+    plt.hist(act_pre_A_on.cpu().numpy(), bins=50, alpha=0.5, label='Active On', density=True)
+    plt.hist(ina_pre_A_on.cpu().numpy(), bins=50, alpha=0.5, label='Inactive On', density=True)
+
+    plt.xlim(-2,4)
+    plt.axvline(x=0, color='k', linestyle='--')
+    plt.legend()
+
+    plt.subplot(2, 1, 2)
+    plt.hist(act_pre_A_x.cpu().numpy(), bins=50, alpha=0.5, label='Active X', density=True)
+    plt.hist(ina_pre_A_x.cpu().numpy(), bins=50, alpha=0.5, label='Inactive X', density=True)
+    plt.hist(act_pre_A_y.cpu().numpy(), bins=50, alpha=0.5, label='Active Y', density=True)
+    plt.hist(ina_pre_A_y.cpu().numpy(), bins=50, alpha=0.5, label='Inactive Y', density=True)
+
+    plt.xlim(-2,4)
+    plt.axvline(x=0, color='k', linestyle='--')
+    plt.legend()
+
+    plt.show()
+
+#%%
+mask = torch.zeros(bs,T, dtype=torch.bool, device=device)
+for batch in range(bs):
+    mask[batch, active_circuits[batch]] = True
+
+
+
+
+#%%
 
 Dod = 100
 D = 300
