8c191eca2abd0c88009262442878c3ace4f7286f
Linda <linda.linsefors@gmail.com>
2025-08-13 20:50:44 +0100

    Added and tested IdealCompInSup

--- ideal_rot_in_sup.py
+++ ideal_rot_in_sup.py
@@ -6,7 +6,6 @@ import matplotlib.pyplot as plt
 import torch
 import tqdm
 
-from networks import RunData
 
 device = 'cpu' 
 torch.set_default_device(device)
@@ -122,9 +121,196 @@ class IdealRotInSup:
         run.x = x
 
         return run
+    
+
+class IdealCompInSup:
+    def __init__(self, Dod, L, S, small_circuits, correction=None):
+        self.Dod = Dod
+        self.L = L
+        self.S = S
+        self.small_circuits = small_circuits
+        self.correction = correction
+        self.T = small_circuits.T
+        self.w = small_circuits.w
+
+        T = self.T
+        w = self.w
+
+        embed = torch.zeros(L, T, Dod)
+        unemb = torch.zeros(L, T, Dod)
+        assign = torch.zeros(L, T, S, dtype=torch.int64)
+
+        embed[0], assign[0] = comp_in_sup_assignment(T, Dod, S)
+        for l in range(1, L):
+            shuffle = torch.randperm(T)
+            embed[l] = embed[0][shuffle]
+            assign[l] = assign[0][shuffle]
+
+        unemb = - torch.ones(L, T, Dod) * correction
+        unemb += embed * (1/S + correction)
+
+        W = torch.zeros(L, 3*Dod, 3*Dod)
+        W[0] = torch.eye(3*Dod)
+
+        for l in range(1,L):
+            [[W[l,:Dod,     :Dod], W[l,:Dod,     Dod:2*Dod], W[l,:Dod,     2*Dod:]],
+             [W[l,Dod:2*Dod,:Dod], W[l,Dod:2*Dod,Dod:2*Dod], W[l,Dod:2*Dod,2*Dod:]],
+             [W[l,2*Dod:,   :Dod], W[l,2*Dod:,   Dod:2*Dod], W[l,2*Dod:,   2*Dod:]]
+            ]= torch.einsum('tn,tij,tm->ijnm', unemb[l], w, embed[l-1])
+
+
+        B = torch.zeros(L, D, device=device)
+        B[1:, :Dod] = small_circuits.b[0]
+        B[1:, Dod:2*Dod] = small_circuits.b[1]
+        B[1:, 2*Dod:] = small_circuits.b[2]
+
+        self.embed = embed
+        self.unemb = unemb
+        self.assign = assign
+
+        self.W = W
+        self.B = B
+
+    def run(self, z, bs):
+
+        Dod = self.Dod
+        L = self.L
+        T = self.T
+        S = self.S
+        embed = self.embed
+        W = self.W
+        B = self.B
+
+        a, active_circuits = self.small_circuits.run(L, z, bs)
+        x = a[:, :, :, 1:] - 1
+
+        A = torch.zeros(L+1, bs, 3*Dod)
+        pre_A = torch.zeros(L+1, bs, 3*Dod)
+        no_msk_A = torch.zeros(L+1, bs, 3*Dod)
+
+        [A[0,:,:Dod], A[0,:,Dod:2*Dod], A[0,:,2*Dod:]] = torch.einsum('btn,bti->ibn', embed[0, active_circuits], a[1])
+        no_msk_A[0] = A[0]
+        pre_A[0] = A[0]
+
+        for l in range(L):
+            pre_A[l+1] = torch.einsum('nm,bm->bn', W[l], A[l]) + B[l]
+            no_msk_A[l+1] = torch.einsum('nm,bm->bn', W[l], no_msk_A[l]) + B[l]
+
+            mask = torch.einsum('btn->bn', embed[l, active_circuits]) != 0
+            A[l+1, :, :Dod     ][mask] = pre_A[l+1, :, :Dod     ][mask]
+            A[l+1, :, Dod:2*Dod][mask] = pre_A[l+1, :, Dod:2*Dod][mask]
+            A[l+1, :, 2*Dod:   ][mask] = pre_A[l+1, :, 2*Dod:   ][mask]
+
+        est_a = torch.zeros(L+1, bs, z, 3)
+        no_msk_est_a = torch.zeros(L+1, bs, z, 3)
+
+        est_a[0] = a[0]
+        no_msk_est_a[0] = a[0]
+
+        for l in range(L):
+            est_a[l+1, :, :, 0] = torch.einsum('btn,bn->bt', self.unemb[l, active_circuits], A[l+1, :, :Dod     ])
+            est_a[l+1, :, :, 1] = torch.einsum('btn,bn->bt', self.unemb[l, active_circuits], A[l+1, :, Dod:2*Dod])
+            est_a[l+1, :, :, 2] = torch.einsum('btn,bn->bt', self.unemb[l, active_circuits], A[l+1, :, 2*Dod:   ])
+
+            no_msk_est_a[l+1, :, :, 0] = torch.einsum('btn,bn->bt', self.unemb[l, active_circuits], no_msk_A[l+1, :, :Dod     ])
+            no_msk_est_a[l+1, :, :, 1] = torch.einsum('btn,bn->bt', self.unemb[l, active_circuits], no_msk_A[l+1, :, Dod:2*Dod])
+            no_msk_est_a[l+1, :, :, 2] = torch.einsum('btn,bn->bt', self.unemb[l, active_circuits], no_msk_A[l+1, :, 2*Dod:   ])
+
+        est_x = torch.zeros(L+1, bs, z, 2)
+        no_msk_est_x = torch.zeros(L+1, bs, z, 2)
+
+        est_x[0] = x[0]
+        no_msk_est_x[0] = x[0]
+
+        est_x[1:, :, :, 0] = est_a[1:, :, :, 1] - est_a[1:, :, :, 0]
+        est_x[1:, :, :, 1] = est_a[1:, :, :, 2] - est_a[1:, :, :, 0]
+
+        no_msk_est_x[1:, :, :, 0] = no_msk_est_a[1:, :, :, 1] - no_msk_est_a[1:, :, :, 0]
+        no_msk_est_x[1:, :, :, 1] = no_msk_est_a[1:, :, :, 2] - no_msk_est_a[1:, :, :, 0]
+
+        run = RunData()
+
+        run.A = A
+        run.pre_A = pre_A
+        run.no_msk_A = no_msk_A
+
+        run.a = a
+        run.est_a = est_a
+        run.no_msk_est_a = no_msk_est_a
+
+        run.est_x = est_x
+        run.no_msk_est_x = no_msk_est_x
+
+        run.active_circuits = active_circuits
+        run.x = x
+
+        return run
+
+
 
 #%% Plot
 #   Plot
+
+
+D = 1200
+T = 1000
+
+S = 5
+z = 3
+bs = 800
+L = 6
+Dod = D // 3
+b=1
+
+circ = RotSmallCircuits(T, b)
+
+version = 'Ideal Comp-in-Sup'
+#version = 'Ideal Rot-in-Sup'
+
+if version == 'Ideal Rot-in-Sup':
+    NetClass = IdealRotInSup
+if version == 'Ideal Comp-in-Sup':
+    NetClass = IdealCompInSup
+
+# No special unembed
+correction = 0
+net = NetClass(Dod, L, S, circ, correction)
+run = net.run(z, bs)
+
+mse = (run.est_x - run.x).pow(2).mean(dim=(1, 2)).sum(dim=-1)
+no_mask_mse = (run.no_msk_est_x - run.x).pow(2).mean(dim=(1, 2)).sum(dim=-1)
+
+plt.plot(mse.cpu().numpy(), marker='o', label='normal')
+#plt.plot(no_mask_mse.cpu().numpy(), marker='o', label='No Mask')
+
+# Special unembed
+correction = 1/(Dod-S)
+net = NetClass(Dod, L, S, circ, correction)
+run = net.run(z, bs)
+
+mse = (run.est_x - run.x).pow(2).mean(dim=(1, 2)).sum(dim=-1)
+no_mask_mse = (run.no_msk_est_x - run.x).pow(2).mean(dim=(1, 2)).sum(dim=-1)
+
+plt.plot(mse.cpu().numpy(), marker='o', label='Mod Un-embed')
+#plt.plot(no_mask_mse.cpu().numpy(), marker='o', label='No Mask & Mod Un-embed')
+
+# Expected error
+plt.plot([expected_mse(T,Dod,l,b)[1] for l in range(L+1)], linestyle='--', marker='x', label='Expected MSE')
+
+# Other plot stuff
+plt.grid(True)
+plt.xlabel('Layer')
+plt.xticks(range(L+1))
+plt.legend()
+
+plt.title(f'{version}\n Dod={Dod}, T={T}, S={S}, z={z}, bs={bs}')
+
+plt.show()
+
+
+
+
+#%%
 
 
 D = 1200
@@ -135,27 +321,81 @@ z = 1
 bs = 800
 L = 4
 Dod = D // 3
+b = 1
 
+circ = RotSmallCircuits(T, b)
+
+version = 'Ideal Comp-in-Sup'
+
+# No special unembed
 correction = 0
-
-circ = RotSmallCircuits(T, 0)
-net = IdealRotInSup(Dod, L, S, circ, correction)
+net = NetClass(Dod, L, S, circ, correction)
 run = net.run(z, bs)
 
-mse = (run.est_x - run.x).pow(2).mean(dim=(1, 2)).sum(dim=-1)
-no_mask_mse = (run.no_msk_est_x - run.x).pow(2).mean(dim=(1, 2)).sum(dim=-1)
+on = run.a[:, :, :, 0]
+est_on = run.est_a[:, :, :, 0]
+no_mask_est_on = run.no_msk_est_a[:, :, :, 0]
 
-plt.plot(mse.cpu().numpy(), marker='o', label='MSE')
-plt.plot(no_mask_mse.cpu().numpy(), marker='o', label='No Mask MSE')
-plt.plot([expected_mse(T,Dod,l,0)[1] for l in range(L+1)], linestyle='--', marker='x', label='Expected MSE')
+mse = (est_on - on).pow(2).mean(dim=(1, 2))
+no_mask_mse = (no_mask_est_on - on).pow(2).mean(dim=(1, 2))
+
+#plt.plot(mse.cpu().numpy(), marker='o', label='normal')
+#plt.plot(no_mask_mse.cpu().numpy(), marker='o', label='No Mask')
+
+# Special unembed
+correction = 1/(Dod-S)
+net = NetClass(Dod, L, S, circ, correction)
+run = net.run(z, bs)
+
+on = run.a[:, :, :, 0]
+est_on = run.est_a[:, :, :, 0]
+no_mask_est_on = run.no_msk_est_a[:, :, :, 0]
+
+mse = (est_on - on).pow(2).mean(dim=(1, 2))
+no_mask_mse = (no_mask_est_on - on).pow(2).mean(dim=(1, 2))
+
+plt.plot(mse.cpu().numpy(), marker='o', label='Mod Un-embed')
+#plt.plot(no_mask_mse.cpu().numpy(), marker='o', label='No Mask & Mod Un-embed')
+
+# Expected error
+plt.plot([expected_mse(T,Dod,l,b)[1] for l in range(L+1)], linestyle='--', marker='x', label='Expected MSE')
+
+# Other plot stuff
 plt.grid(True)
 plt.xlabel('Layer')
 plt.xticks(range(L+1))
 plt.legend()
 
-plt.title(f'Dod={Dod}, T={T}, S={S}, z={z}, bs={bs}')
+plt.title(f'{version}\n Dod={Dod}, T={T}, S={S}, z={z}, bs={bs}, b={b}')
 
 plt.show()
+#%%
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
